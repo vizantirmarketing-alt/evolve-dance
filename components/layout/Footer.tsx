79c561f773c49a507562e63cc6ca19a86d267987
James Tram <vizantirmarketing@gmail.com>
2026-05-14 08:35:10 -0700

    Footer: remove glitchy logo animation, keep link to homepage

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -109,14 +109,14 @@ export default function Footer() {
                 <Link
                   href="/"
                   aria-label="Evolve Dance Center home"
-                  className="group flex items-center no-underline transition-transform duration-200 ease-out hover:scale-[1.03]"
+                  className="flex items-center no-underline"
                 >
                   <Image
                     src="/logo/evolve-footer.png"
                     alt="Evolve Dance Center"
                     width={360}
                     height={240}
-                    className="h-16 md:h-20 w-auto object-contain transition-opacity duration-200 ease-out group-hover:opacity-85"
+                    className="h-16 md:h-20 w-auto object-contain"
                   />
                 </Link>
               </div>
